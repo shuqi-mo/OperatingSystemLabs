b0efef3291d0fdfd2bcc4ee38092459e8fb1371c
shuqi-mo <moshq7@foxmail.com>
2021-04-15 20:57:45 +0800

    Complete!

--- lab2/lab2-sysu/kern/process/proc.c
+++ lab2/lab2-sysu/kern/process/proc.c
@@ -308,34 +308,34 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    5. insert proc_struct into hash_list && proc_list
     //    6. call wakup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
-if((proc = alloc_proc()) == NULL)
-    goto fork_out;
-proc->parent = current;
-if(setup_kstack(proc) != 0)
-    goto bad_fork_cleanup_proc;
-if(copy_mm(clone_flags, proc) != 0)
-    goto bad_fork_cleanup_kstack;
-copy_thread(proc, stack, tf);
-bool intr_flag;
-local_intr_save(intr_flag);
-{
-    proc->pid = get_pid();
-    hash_proc(proc);
-    list_add(&proc_list, &(proc->list_link));
-    nr_process++;
-}
-local_intr_restore(intr_flag);
-wakeup_proc(proc);
-ret = proc->pid;
+   if((proc = alloc_proc()) == NULL)
+       goto fork_out;
+   proc->parent = current;
+   if(setup_kstack(proc) != 0)
+       goto bad_fork_cleanup_proc;
+   if(copy_mm(clone_flags, proc) != 0)
+       goto bad_fork_cleanup_kstack;
+   copy_thread(proc, stack, tf);
+   bool intr_flag;
+   local_intr_save(intr_flag);
+   {
+       proc->pid = get_pid();
+       hash_proc(proc);
+       list_add(&proc_list, &(proc->list_link));
+       nr_process++;
+   }
+   local_intr_restore(intr_flag);
+   wakeup_proc(proc);
+   ret = proc->pid;
 
-fork_out:
-    return ret;
+   fork_out:
+       return ret;
 
-bad_fork_cleanup_kstack:
-    put_kstack(proc);
-bad_fork_cleanup_proc:
-    kfree(proc);
-    goto fork_out;
+   bad_fork_cleanup_kstack:
+       put_kstack(proc);
+   bad_fork_cleanup_proc:
+       kfree(proc);
+       goto fork_out;
 }
 
 // do_exit - called by sys_exit
